90134683586d989ca47a6792d41a2b6d5501669a
houpengtao <houpengtao@huawei.com>
2022-07-09 18:45:47 +0800

    文件下载失败回调函数参数个数保持和接口一致
    Signed-off-by: houpengtao <houpengtao@huawei.com>

--- download/services/src/download_service_task.cpp
+++ download/services/src/download_service_task.cpp
@@ -216,7 +216,7 @@ void DownloadServiceTask::SetStatus(DownloadStatus status, ErrorCode code, Pause
             }
         }
 
-        return true;
+        return isChanged;
     };
     DOWNLOAD_HILOGI("Status [%{public}d], Code [%{public}d], Reason [%{public}d]", status, code, reason);
     if (!stateChange(status, code, reason)) {
