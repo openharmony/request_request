639abbe184a573cd082db04f34203c980f6b0564
anyueling <anyueling@huawei.com>
2022-09-21 20:22:35 +0800

    告警清零
    Signed-off-by: anyueling <anyueling@huawei.com>

--- upload/frameworks/libs/src/module_init.cpp
+++ upload/frameworks/libs/src/module_init.cpp
@@ -43,16 +43,21 @@ static unsigned long ThreadIdCallback(void)
     return ret;
 }
 
+using THREAD_ID_CALLBACK = unsigned long (*)(void);
+using LOCK_CALLBACK = void (*)(int mode, int type, char *file, int line);
 static void InitLocks(void)
 {
-
+    THREAD_ID_CALLBACK threadIdCallback;
+    LOCK_CALLBACK lockCallback;
+    threadIdCallback = ThreadIdCallback;
+    lockCallback = LockCallback;
     lockarray = (pthread_mutex_t *)OPENSSL_malloc(CRYPTO_num_locks() *
                                                 sizeof(pthread_mutex_t));
     for (int i = 0; i<CRYPTO_num_locks(); i++) {
         pthread_mutex_init(&(lockarray[i]), NULL);
     }
-    CRYPTO_set_id_callback(ThreadIdCallback);
-    CRYPTO_set_locking_callback(LockCallback);
+    CRYPTO_set_id_callback(threadIdCallback);
+    CRYPTO_set_locking_callback(lockCallback);
 }
 
 static void KillLocks(void)
@@ -88,4 +93,4 @@ ModuleInit::~ModuleInit()
     curl_global_cleanup();
 }
 
-__attribute__((used)) static ModuleInit mi;
+static ModuleInit mi;
