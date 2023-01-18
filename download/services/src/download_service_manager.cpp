4c2d32292710ed8251ec8b04f86e973ee52ba6b6
Yangys <yangyousheng@huawei.com>
2023-01-18 19:15:56 +0800

    Modify ErrorCode
    Signed-off-by: Yangys <yangyousheng@huawei.com>

--- download/services/src/download_service_manager.cpp
+++ download/services/src/download_service_manager.cpp
@@ -95,7 +95,7 @@ bool DownloadServiceManager::Create(uint32_t threadNum)
         int retryCount = 0;
         constexpr int RETRY_TIME_INTERVAL_MILLISECOND = 1 * 1000 * 1000; // retry after 1 second
         do {
-            if (this->MonitorNetwork() == NET_CONN_SUCCESS) {
+            if (this->MonitorNetwork() == NETMANAGER_SUCCESS) {
                 break;
             }
             retryCount++;
