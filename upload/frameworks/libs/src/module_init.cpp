e87b13ce4bfb91f9785e3d3d40d65d1203ea94e0
wTong6 <wangtong94@huawei.com>
2023-03-17 10:07:45 +0800

    modify warning
    Signed-off-by: wTong6 <wangtong94@huawei.com>

--- upload/frameworks/libs/src/module_init.cpp
+++ upload/frameworks/libs/src/module_init.cpp
@@ -12,15 +12,15 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
- 
+
+
 #define USE_OPENSSL
 
 #include <curl/curl.h>
 #include <pthread.h>
 #include <cstddef>
 #include "upload_hilog_wrapper.h"
-static pthread_mutex_t *lockArray = nullptr;
+static pthread_mutex_t *g_lockArray = nullptr;
 
 #ifdef USE_OPENSSL
 #include <openssl/crypto.h>
@@ -31,9 +31,9 @@ static void LockCallback(int mode, int type, char *file, int line)
     (void)file;
     (void)line;
     if (mode & CRYPTO_LOCK) {
-        pthread_mutex_lock(&(lockArray[type]));
+        pthread_mutex_lock(&(g_lockArray[type]));
     } else {
-        pthread_mutex_unlock(&(lockArray[type]));
+        pthread_mutex_unlock(&(g_lockArray[type]));
     }
 }
 
@@ -51,14 +51,14 @@ static void InitLocks(void)
     LOCK_CALLBACK lockCallback;
     threadIdCallback = ThreadIdCallback;
     lockCallback = LockCallback;
-    lockArray = (pthread_mutex_t *)OPENSSL_malloc(CRYPTO_num_locks() *
+    g_lockArray = (pthread_mutex_t *)OPENSSL_malloc(CRYPTO_num_locks() *
                                                 sizeof(pthread_mutex_t));
-    if (lockArray == nullptr) {
+    if (g_lockArray == nullptr) {
         UPLOAD_HILOGD(UPLOAD_MODULE_JS_NAPI, "failed to create openssl lock");
         return;
     }
     for (int i = 0; i < CRYPTO_num_locks(); i++) {
-        pthread_mutex_init(&(lockArray[i]), NULL);
+        pthread_mutex_init(&(g_lockArray[i]), nullptr);
     }
     CRYPTO_set_id_callback(threadIdCallback);
     CRYPTO_set_locking_callback(lockCallback);
@@ -69,9 +69,9 @@ static void KillLocks(void)
     int i;
     CRYPTO_set_locking_callback(NULL);
     for (i = 0; i < CRYPTO_num_locks(); i++) {
-        pthread_mutex_destroy(&(lockArray[i]));
+        pthread_mutex_destroy(&(g_lockArray[i]));
     }
-    OPENSSL_free(lockArray);
+    OPENSSL_free(g_lockArray);
 }
 #endif
 
