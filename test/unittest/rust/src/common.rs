620d24124ead4f3c05c2f151a2bd639ddbe543e6
qimeng <qimeng11@huawei.com>
2023-05-22 15:06:30 +0800

    gn format
    Signed-off-by: qimeng <qimeng11@huawei.com>

--- test/unittest/rust/src/common.rs
+++ test/unittest/rust/src/common.rs
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 #![allow(unused_must_use)]
 extern crate request;
 
--- test/unittest/rust/src/main.rs
+++ test/unittest/rust/src/main.rs
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 #![allow(unused_assignments)]
 mod common;
 extern crate request;
