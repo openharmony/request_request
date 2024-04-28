8d24bf5eb506c5fb138441f77a1e8e0f8032b1f3
zhouqingze <zhouqingze@huawei.com>
2024-04-28 15:44:12 +0800

    Signed-off-by: zhouqingze <zhouqingze@huawei.com> Change-Id: I9ddcc4227f22c09a9ea2fea07230e996e62ad2b3
    Signed-off-by: zhouqingze <zhouqingze@huawei.com>
    Change-Id: I6d9eb5e72cdfc7f183e0a50173acadcc384a865c
    Signed-off-by: zhouqingze <zhouqingze@huawei.com>

---
+++ test/unittest/cpp_test/fwkTest/src/request_sync_load_callback_test.cpp
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define private public
+#define protected public
+
+#include "request_sync_load_callback.h"
+
+#include <gtest/gtest.h>
+
+#include <cstdint>
+#include <memory>
+#include <vector>
+
+#include "gmock/gmock.h"
+#include "js_common.h"
+#include "log.h"
+#include "refbase.h"
+#include "system_ability_definition.h"
+
+using namespace testing::ext;
+using namespace OHOS::Request;
+
+#undef private
+#undef protected
+
+class RequestSyncLoadCallbackTest : public testing::Test {
+public:
+    static void SetUpTestCase(void);
+    static void TearDownTestCase(void);
+    void SetUp();
+    void TearDown();
+};
+
+void RequestSyncLoadCallbackTest::SetUpTestCase(void)
+{
+    // input testSuit setup step，setup invoked before all testCases
+}
+
+void RequestSyncLoadCallbackTest::TearDownTestCase(void)
+{
+    // input testSuit teardown step，teardown invoked after all testCases
+}
+
+void RequestSyncLoadCallbackTest::SetUp(void)
+{
+    // input testCase setup step，setup invoked before each testCase
+    testing::UnitTest *test = testing::UnitTest::GetInstance();
+    ASSERT_NE(test, nullptr);
+    const testing::TestInfo *testInfo = test->current_test_info();
+    ASSERT_NE(testInfo, nullptr);
+    string testCaseName = string(testInfo->name());
+    REQUEST_HILOGI("[SetUp] %{public}s start", testCaseName.c_str());
+    GTEST_LOG_(INFO) << testCaseName.append(" start");
+}
+
+void RequestSyncLoadCallbackTest::TearDown(void)
+{
+    // input testCase teardown step，teardown invoked after each testCase
+}
+
+class RSLCTRemoteObjectImpl : public OHOS::IRemoteObject {};
+
+HWTEST_F(RequestSyncLoadCallbackTest, OnLoadSystemAbility001, TestSize.Level1)
+{
+    OHOS::sptr<RSLCTRemoteObjectImpl> remote;
+    RequestSyncLoadCallback requestSyncLoadCallback = RequestSyncLoadCallback();
+    requestSyncLoadCallback.OnLoadSystemAbilityFail(OHOS::PRINT_SERVICE_ID);
+    requestSyncLoadCallback.OnLoadSystemAbilityFail(OHOS::DOWNLOAD_SERVICE_ID);
+    requestSyncLoadCallback.OnLoadSystemAbilitySuccess(OHOS::PRINT_SERVICE_ID, remote);
+    requestSyncLoadCallback.OnLoadSystemAbilitySuccess(OHOS::DOWNLOAD_SERVICE_ID, remote);
+}
---
+++ test/unittest/cpp_test/fwkTest/src/request_test.cpp
@@ -0,0 +1,162 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define private public
+#define protected public
+#include "request.h"
+
+#include <gtest/gtest.h>
+
+#include <memory>
+
+#include "gmock/gmock.h"
+#include "js_common.h"
+#include "log.h"
+
+using namespace testing::ext;
+using namespace OHOS::Request;
+
+#undef private
+#undef protected
+
+int g_requestTest = 0;
+
+class RequestTest : public testing::Test {
+public:
+    static void SetUpTestCase(void);
+    static void TearDownTestCase(void);
+    void SetUp();
+    void TearDown();
+};
+
+void RequestTest::SetUpTestCase(void)
+{
+    // input testSuit setup step，setup invoked before all testCases
+}
+
+void RequestTest::TearDownTestCase(void)
+{
+    // input testSuit teardown step，teardown invoked after all testCases
+}
+
+void RequestTest::SetUp(void)
+{
+    // input testCase setup step，setup invoked before each testCase
+    testing::UnitTest *test = testing::UnitTest::GetInstance();
+    ASSERT_NE(test, nullptr);
+    const testing::TestInfo *testInfo = test->current_test_info();
+    ASSERT_NE(testInfo, nullptr);
+    string testCaseName = string(testInfo->name());
+    REQUEST_HILOGI("[SetUp] %{public}s start", testCaseName.c_str());
+    GTEST_LOG_(INFO) << testCaseName.append(" start");
+}
+
+void RequestTest::TearDown(void)
+{
+    // input testCase teardown step，teardown invoked after each testCase
+}
+
+HWTEST_F(RequestTest, GetIdTest001, TestSize.Level1)
+{
+    string tid = "testTid";
+    Request request = Request(tid);
+    EXPECT_EQ(request.getId(), tid);
+}
+
+class RTResponseListenerImpl : public IResponseListener {
+public:
+    ~RTResponseListenerImpl(){};
+    void OnResponseReceive(const std::shared_ptr<Response> &response) override
+    {
+        (void)response;
+        g_requestTest = 2;
+        return;
+    }
+};
+
+HWTEST_F(RequestTest, AddAndRemoveListenerTest001, TestSize.Level1)
+{
+    string tid = "testTid";
+    SubscribeType type = SubscribeType::RESPONSE;
+    Request request = Request(tid);
+    std::shared_ptr<RTResponseListenerImpl> listenerPtr = std::make_shared<RTResponseListenerImpl>();
+    request.AddListener(type, listenerPtr);
+    EXPECT_EQ(request.HasListener(), true);
+    request.RemoveListener(type, listenerPtr);
+    EXPECT_EQ(request.HasListener(), false);
+}
+
+class RTNotifyDataListenerImpl : public INotifyDataListener {
+public:
+    ~RTNotifyDataListenerImpl(){};
+    void OnNotifyDataReceive(const std::shared_ptr<NotifyData> &notifyData) override
+    {
+        (void)notifyData;
+        g_requestTest = 1;
+        return;
+    }
+};
+
+HWTEST_F(RequestTest, OnNotifyDataReceiveTest001, TestSize.Level1)
+{
+    g_requestTest = 0;
+    string tid = "testTid";
+    SubscribeType type = SubscribeType::COMPLETED;
+    Request request = Request(tid);
+    std::shared_ptr<NotifyData> notifyData = std::make_shared<NotifyData>();
+    notifyData->type = type;
+    notifyData->version = Version::API9;
+    request.OnNotifyDataReceive(notifyData);
+    EXPECT_EQ(g_requestTest, 0);
+    std::shared_ptr<RTNotifyDataListenerImpl> listenerPtr = std::make_shared<RTNotifyDataListenerImpl>();
+    request.AddListener(type, listenerPtr);
+    EXPECT_EQ(request.HasListener(), true);
+    request.OnNotifyDataReceive(notifyData);
+    EXPECT_EQ(g_requestTest, 1);
+}
+
+HWTEST_F(RequestTest, AddAndRemoveListenerTest002, TestSize.Level1)
+{
+    g_requestTest = 0;
+    string tid = "testTid";
+    SubscribeType type = SubscribeType::COMPLETED;
+    Request request = Request(tid);
+    std::shared_ptr<NotifyData> notifyData = std::make_shared<NotifyData>();
+    notifyData->type = type;
+    notifyData->version = Version::API9;
+    request.OnNotifyDataReceive(notifyData);
+    std::shared_ptr<RTNotifyDataListenerImpl> listenerPtr = std::make_shared<RTNotifyDataListenerImpl>();
+    request.AddListener(type, listenerPtr);
+    EXPECT_EQ(request.HasListener(), true);
+    EXPECT_EQ(g_requestTest, 1);
+    request.RemoveListener(type, listenerPtr);
+    EXPECT_EQ(request.HasListener(), false);
+}
+
+HWTEST_F(RequestTest, OnResponseReceiveTest002, TestSize.Level1)
+{
+    g_requestTest = 0;
+    string tid = "testTid";
+    SubscribeType type = SubscribeType::RESPONSE;
+    std::shared_ptr<Response> response = std::make_shared<Response>();
+    Request request = Request(tid);
+    request.OnResponseReceive(response);
+    EXPECT_EQ(g_requestTest, 0);
+    std::shared_ptr<RTResponseListenerImpl> listenerPtr = std::make_shared<RTResponseListenerImpl>();
+    request.AddListener(type, listenerPtr);
+    EXPECT_EQ(request.HasListener(), true);
+    request.OnResponseReceive(response);
+    EXPECT_EQ(g_requestTest, 2);
+}
