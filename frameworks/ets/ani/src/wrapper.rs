fe0211456bc76b9e9eb512268b4b322bc12cac3d
fqwert <yanglv2@huawei.com>
2025-05-07 17:49:24 +0800

    connection_ani
    Signed-off-by: fqwert <yanglv2@huawei.com>
    Change-Id: Ie4f6b60acf185c61801a4a3996ac3d039c2f9906

---
+++ frameworks/ets/ani/src/wrapper.rs
@@ -0,0 +1,47 @@
+// Copyright (C) 2025 Huawei Device Co., Ltd.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use crate::listener::UdsListener;
+
+fn on_response(response: ffi::Response) {
+    info!(
+        "on_response: taskId: {}, version: {}, statusCode: {}, reason: {}, headers: {:?}",
+        response.taskId, response.version, response.statusCode, response.reason, response.headers
+    );
+    UdsListener::get_instance().on_response(response);
+}
+
+#[cxx::bridge(namespace = "OHOS::RequestAni")]
+pub mod ffi {
+
+    struct Response {
+        taskId: String,
+        version: String,
+        statusCode: i32,
+        reason: String,
+        headers: Vec<String>,
+    }
+
+    extern "Rust" {
+        fn on_response(response: Response);
+    }
+
+    unsafe extern "C++" {
+        include!("subscribe.h");
+        include!("wrapper.h");
+
+        fn GetAppBaseDir() -> String;
+        fn AclSetAccess(target: &str, entry: &str) -> i32;
+        fn OpenChannel(fd: i32);
+    }
+}
