53a9b6325eff31ce4bdc078a0e14a4326f1eb0f4
Tiga Ultraman <liaoxude@huawei.com>
2025-08-28 16:24:44 +0800

    inotify监控历史目录删除
    Signed-off-by: Tiga Ultraman <liaoxude@huawei.com>
    Change-Id: I468ad29e79e684b674c512f33bc2c4cf64c1f3d8

---
+++ frameworks/native/cache_core/src/data/observer.rs
@@ -0,0 +1,46 @@
+// Copyright (C) 2025 Huawei Device Co., Ltd.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::{fs, path::PathBuf, sync::Arc};
+
+use crate::data::file::HistoryDir;
+
+pub struct DirRebuilder {
+    curr: PathBuf,
+    history: Arc<HistoryDir>,
+}
+
+impl DirRebuilder {
+    pub fn new(curr: PathBuf, history: Arc<HistoryDir>) -> Self {
+        Self { curr, history }
+    }
+
+    pub fn remove_store_dir(&self) {
+        if self.curr.is_dir() {
+            // Don't care about the failed deletion.
+            if let Err(e) = fs::remove_dir_all(self.curr.as_path()) {
+                error!("remove local store directory fail, err: {:?}", e);
+            };
+        }
+    }
+
+    pub fn stop_history_observe(&self) {
+        self.history.stop_observe();
+    }
+}
+
+impl Drop for DirRebuilder {
+    fn drop(&mut self) {
+        self.stop_history_observe();
+    }
+}
