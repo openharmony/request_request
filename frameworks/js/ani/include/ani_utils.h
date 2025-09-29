6ae923c342684d5179b77060f46b8998a2ba9b11
xingzeng <xingzeng2@huawei.com>
2025-09-29 18:49:25 +0800

    Remove unboxed method callings
    Issue: https://gitcode.com/openharmony/arkcompiler_runtime_core/issues/7079
    
    Reason: The es2panda front-end emits toByte&co instead of unboxed in lowering.
    The public unboxed() method is slated for removal now that we don't have
    primitives at the language level.
    
    Meaning: Transfer from relying on primitive types to not relying on primitive
    types, with promise that in new language compiling framework it stiil can
    process type cast correctly.
    
    Signed-off-by: xingzeng <xingzeng2@huawei.com>

--- frameworks/js/ani/include/ani_utils.h
+++ frameworks/js/ani/include/ani_utils.h
@@ -244,7 +244,7 @@ inline bool UnionAccessor::TryConvert<bool>(bool &value)
     }
 
     ani_boolean aniValue;
-    auto ret = env_->Object_CallMethodByName_Boolean(obj_, "unboxed", nullptr, &aniValue);
+    auto ret = env_->Object_CallMethodByName_Boolean(obj_, "toBoolean", nullptr, &aniValue);
     if (ret != ANI_OK) {
         return false;
     }
@@ -260,7 +260,7 @@ inline bool UnionAccessor::TryConvert<int>(int &value)
     }
 
     ani_int aniValue;
-    auto ret = env_->Object_CallMethodByName_Int(obj_, "unboxed", nullptr, &aniValue);
+    auto ret = env_->Object_CallMethodByName_Int(obj_, "toInt", nullptr, &aniValue);
     if (ret != ANI_OK) {
         return false;
     }
@@ -276,7 +276,7 @@ inline bool UnionAccessor::TryConvert<double>(double &value)
     }
 
     ani_double aniValue;
-    auto ret = env_->Object_CallMethodByName_Double(obj_, "unboxed", nullptr, &aniValue);
+    auto ret = env_->Object_CallMethodByName_Double(obj_, "toDouble", nullptr, &aniValue);
     if (ret != ANI_OK) {
         return false;
     }
@@ -347,7 +347,7 @@ inline expected<bool, ani_status> OptionalAccessor::Convert<bool>()
     }
 
     ani_boolean aniValue;
-    auto ret = env_->Object_CallMethodByName_Boolean(obj_, "unboxed", nullptr, &aniValue);
+    auto ret = env_->Object_CallMethodByName_Boolean(obj_, "toBoolean", nullptr, &aniValue);
     if (ret != ANI_OK) {
         return ret;
     }
@@ -533,7 +533,7 @@ struct ToDouble {
     {
         ani_double aniValue;
         ani_object itemObj = static_cast<ani_object>(itemRef);
-        ani_status status = env->Object_CallMethodByName_Double(itemObj, "unboxed", nullptr, &aniValue);
+        ani_status status = env->Object_CallMethodByName_Double(itemObj, "toDouble", nullptr, &aniValue);
         if (ANI_OK != status) {
             value = static_cast<double>(aniValue);
         }
